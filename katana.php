993ca884b6f9352ac812b646d586e4017a4a7963
PachydermX <renjimin@gmail.com>
2016-02-09 00:59:38 +0900

    PHP Convert

---
+++ katana.php
@@ -0,0 +1,414 @@
+<?php
+/**
+ * Template Name: Keynote
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,
+                               maximum-scale=1.0" />
+<html>
+<head>
+    <!-- import jquery library -->
+    <title></title>
+    <script src="jquery-2.1.4.min.js"></script>
+    <script src="jquery.rotate.js"></script>
+    <script src="jquery-ui-1.11.4/jquery-ui.min.js"></script>
+    <script src="jquery.mousewheel.min.js"></script>
+    <script src="jquery.touchSwipe.min.js"></script>
+    <script src="library.js"></script>
+    <script src="meta.js"></script>
+    <script src="configurator.js"></script>
+    <script src="inspector.js"></script>
+    <script src="manager.js"></script>
+    <script src="page.js"></script>
+    <script src="slider.js"></script>
+    <script src="object.js"></script>
+    <link href="jquery-ui-1.11.4/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <link href="katana.css" rel="stylesheet" type="text/css">
+    <link href="keynote.css" rel="stylesheet" type="text/css">
+    <link href="object.css" rel="stylesheet" type="text/css">
+    <link href="inspector.css" rel="stylesheet" type="text/css">
+    <script src="katana.js"></script>
+</head>
+<body>
+	<div id="page_frame">
+		<!-- UI -->
+		<div class="contain arrowui" id="arrowup"></div>
+		<div class="contain arrowui" id="arrowdown"></div>
+		<!-- HOME -->
+		<div class="cover" id="homebg"></div>
+		<div class="contain" id="homelogoa"></div>
+		<div class="homelogolight" id="homelogolighta"></div>
+		<div class="homelogolight" id="homelogolightb"></div>
+		<div class="contain" id="homesakuraa"></div>
+		<div class="contain" id="homesakurab"></div>
+		<div class="contain" id="homesakurac"></div>
+		<div class="top-align contain" id="homekatana"></div>
+		
+		<div class="cover" id="homebgb"></div>
+		<div class="contain" id="homelogob"></div>
+		<div class="preview_container black" id="hometext">
+			<div class="title_container">
+				<span class="title">
+					Welcome to <br>
+					Studio KATANA
+				</span>
+			</div>
+			<hr class="title_divider">
+			<div class="context_container">
+				<p class="context">
+					Combining technology, design & media we build advanced connections between brands and their audiences. We create compelling solutions that deliver and sustain business growth, 
+					<span class="mhhide">starting from web and mobile applications to complex business enterprise software. </span>
+				</p>
+			</div>
+		</div>
+		<div class="contain" id="homesakurad"></div>
+		<div class="contain right-align" id="homesakurae"></div>
+		<div class="contain" id="homesakuraf"></div>
+		
+		<!-- WEBSITE 1-->
+		<div class="cover" id="websitebg"></div>
+		<div id="websitewbg">
+			<span class="middle title">Digitilizing your Business</span>
+		</div>
+		
+		<div id="websiteslider1" class="preview_container center black">
+			<div class="slider_showcase">
+				<div class="slider_icon" id="slider_icon_1"></div>
+			</div>
+			<div class="title_container">
+				<span class="title second">
+					The Company
+				</span>
+			</div>
+			<hr class="title_divider">
+			<div class="context_container">
+				<p class="context">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat 
+				</p>
+			</div>
+			<div class="tag_container">
+				<span class="tag orange">
+					Website
+				</span>
+				<span class="tag green">
+					Interactive
+				</span>
+				<span class="tag purple">
+					Video
+				</span>
+			</div>
+		</div>
+		
+		<div id="websiteslider2" class="preview_container center black">
+			<div class="slider_showcase">
+				<div class="slider_icon" id="slider_icon_2"></div>
+			</div>
+			<div class="title_container">
+				<span class="title second">
+					The Company
+				</span>
+			</div>
+			<hr class="title_divider">
+			<div class="context_container">
+				<p class="context">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat 
+				</p>
+			</div>
+			<div class="tag_container">
+				<span class="tag orange">
+					Website
+				</span>
+				<span class="tag green">
+					Interactive
+				</span>
+				<span class="tag purple">
+					Video
+				</span>
+			</div>
+		</div>
+		
+		<div id="websiteslider3" class="preview_container center black">
+			<div class="slider_showcase">
+				<div class="slider_icon" id="slider_icon_3"></div>
+			</div>
+			<div class="title_container">
+				<span class="title second">
+					The Company
+				</span>
+			</div>
+			<hr class="title_divider">
+			<div class="context_container">
+				<p class="context">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat 
+				</p>
+			</div>
+			<div class="tag_container">
+				<span class="tag orange">
+					Website
+				</span>
+				<span class="tag green">
+					Interactive
+				</span>
+				<span class="tag purple">
+					Video
+				</span>
+			</div>
+		</div>
+		
+		<div id="websiteslider4" class="preview_container center black">
+			<div class="slider_showcase">
+				<div class="slider_icon" id="slider_icon_4"></div>
+			</div>
+			<div class="title_container">
+				<span class="title second">
+					The Company
+				</span>
+			</div>
+			<hr class="title_divider">
+			<div class="context_container">
+				<p class="context">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat 
+				</p>
+			</div>
+			<div class="tag_container">
+				<span class="tag orange">
+					Website
+				</span>
+				<span class="tag green">
+					Interactive
+				</span>
+				<span class="tag purple">
+					Video
+				</span>
+			</div>
+		</div>
+		
+		<div id="websitesliderindicator">
+			<div class="indicator" id="slider_0"></div>
+			<div class="indicator" id="slider_1"></div>
+			<div class="indicator" id="slider_2"></div>
+			<div class="indicator" id="slider_3"></div>
+		</div>
+		
+		<!-- WEBSITE 2 -->
+		<div class="contain" id="websiteleafa"></div>
+		<div class="contain" id="websiteleafb"></div>
+		<div class="contain" id="websiteleafc"></div>
+		<div class="contain" id="websiteleafd"></div>
+		<div class="contain" id="websitedevices"></div>
+		<div class="preview_container" id="websitetext">
+			<div class="tag_container">
+				<span class="tag orange">
+					Website
+				</span>
+			</div>
+			<div class="title_container">
+				<span class="title">
+					Website Creation
+				</span>
+			</div>
+			<hr class="title_divider">
+			<div class="context_container">
+				<p class="context">
+					Combining technology, design & media we build advanced connections between brands and their audiences. We create compelling solutions that deliver and sustain business growth, starting from web and mobile applications to complex business enterprise software.
+				</p>
+			</div>
+			<div class="button_container">
+				<div class="more_button">
+					<div class="vertical_align">
+						<span class="button_label">
+							<span class="button_icon more_info"><img src="katana/moreinfo.png" /></span>
+							More Information
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- INTERECTIVE -->
+		<div class="cover" id="interbg"></div>
+		<div class="interactive circle" id="interinfoa">
+		</div>
+		<div class="interactive circle" id="interinfob">
+		</div>
+		<div class="interactive circle" id="interinfoc">
+		</div>
+		<div class="preview_container black" id="intertext">
+			<div class="tag_container">
+				<span class="tag green">
+					Interactive
+				</span>
+			</div>
+			<div class="title_container">
+				<span class="title">
+					Interactive materials
+				</span>
+			</div>
+			<hr class="title_divider">
+			<div class="context_container">
+				<p class="context">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat fugiat nulla pariatur.
+				</p>
+				<ul class="context mhhide">
+					<li>Identify the different types of websites available</li>
+					<li>develop an online process of selecting and creating a website</li>
+					<li>Login to an online store and explore product management</li>
+					<li>Identify the different on-line and off-line marking strategies</li>
+				</ul>
+			</div>
+			<div class="button_container black_button">
+				<div class="more_button reverse">
+					<div class="vertical_align">
+						<span class="button_label">
+							<span class="button_icon more_info"><img src="katana/moreinfo.png" /></span>
+							More Information
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- BRAND -->
+		<div class="contain" id="brandcarda"></div>
+		<div class="contain" id="brandcardb"></div>
+		<div class="contain" id="brandcardc"></div>
+		<div class="contain bottom-align" id="brandcardd"></div>
+		<div class="contain" id="brandclip"></div>
+		<div class="contain bottom-align" id="brandpen"></div>
+		<div class="preview_container black" id="brandtext">
+			<div class="title_container">
+				<span class="title">
+					Brand Designing
+				</span>
+			</div>
+			<hr class="title_divider">
+			<div class="context_container">
+				<p class="context">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat fugiat nulla pariatur.
+				</p>
+				<ul class="context mhhide">
+					<li>Identify the different types of websites available</li>
+					<li>develop an online process of selecting and creating a website</li>
+					<li>Login to an online store and explore product management</li>
+					<li>Identify the different on-line and off-line marking strategies</li>
+				</ul>
+			</div>
+			<div class="button_container black_button">
+				<div class="more_button reverse">
+					<div class="vertical_align">
+						<span class="button_label">
+							<span class="button_icon more_info"><img src="katana/moreinfo.png" /></span>
+							More Information
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- VIDEO -->
+		<div class="cover" id="videobg"></div>
+		<div class="contain" id="videoclipa"></div>
+		<div class="contain" id="videoclipb"></div>
+		<div class="contain" id="videoclipc"></div>
+		<div class="contain" id="videoclipd"></div>
+		<div class="preview_container" id="videotext">
+			<div class="tag_container">
+				<span class="tag purple">
+					Video
+				</span>
+			</div>
+			<div class="title_container">
+				<span class="title">
+					Video Making
+				</span>
+			</div>
+			<hr class="title_divider">
+			<div class="context_container">
+				<p class="context">
+					Combining technology, design & media we build advanced connections between brands and their audiences. We create compelling solutions that deliver and sustain business growth, starting from web and mobile applications to complex business enterprise software. 
+				</p>
+			</div>
+			<div class="thumb_display mhhide">
+				<div class="thumb_box">
+					<div class="thumb_img">
+						<img class="thumb" src="katana/videosample1.jpg">
+					</div>
+					<div class="thumb_title">
+						Storm Partners
+					</div>
+				</div>
+				<div class="thumb_box">
+					<div class="thumb_img">
+						<img class="thumb" src="katana/videosample2.jpg">
+					</div>
+					<div class="thumb_title">
+						Studio Katana
+					</div>
+				</div>
+			</div>
+			<div class="button_container">
+				<div class="more_button">
+					<div class="vertical_align">
+						<span class="button_label">
+							<span class="button_icon more_info"><img src="katana/moreinfo.png" /></span>
+							More Information
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- SUPPORT -->
+		<div class="cover" id="supportbg"></div>
+		<div id="supportwbg"></div>
+		<div class="title" id="supporttitle">
+			Total Support for you
+		</div>
+		<div class="supporttext" id="supporttexta">
+			<div class="image_container">
+				<img src="katana/supporttexta.png">
+			</div>
+			<p class="context small">
+				Based on a full analysis of your requirements, we uncover the full potential of what your new website can do for you and your business. 
+				<span class="mhhide">We may also offer you a surprize or two which you may dramatically improve your outcome. </span>
+			</p>
+		</div>
+		<div class="supporttext" id="supporttextb">
+			<div class="image_container">
+				<img src="katana/supporttextb.png">
+			</div>
+			<p class="context small">
+			We identify the latest and most suitable web technologies to achieve the result you desire (or better),
+				<span class="mhhide">ensuring the maximum return of your investment whilst providing you the cutting edge online tools for your business. </span>
+			</p>
+		</div>
+		<div class="supporttext" id="supporttextc">
+			<div class="image_container">
+				<img src="katana/supporttextc.png">
+			</div>
+			<p class="context small">
+				Within the first 4 seconds, the visitors to a website decide if they wish to stay or move on to another place in the cyber space. 
+				<span class="mhhide">The Commercial Design Artists at Studio Katana are experts on creating a functional, yet visually attractive websites. We can make your new website the true visual representation of your business, just as unique, just as good looking! </span>
+			</p>
+		</div>
+		<div class="supporttext" id="supporttextd">
+			<div class="image_container">
+				<img src="katana/supporttextd.png">
+			</div>
+			<p class="context small">
+			At Studio Katana, we offer the latest technology but don't charge you an arm and a leg !  
+				<span class="mhhide">To achieve our vision of providing the Global SME community the best online tools at an affordable prices, Studio Katana specializes in creating custom made websites based on WordPress, Joomla and other great cyber tools. What does this mean? Well, if your site was build without taking advantage of those great free resources, it really would cost you an arm and leg. </span>
+			</p>
+		</div>
+		
+		<!-- CONTACT -->
+		<div class="contain" id="contactlogo"></div>
+		<div class="contain" id="contactform"></div>
+	</div>
+</body>
+</html>
