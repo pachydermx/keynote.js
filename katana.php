a5c62cefad3671fa5a9d8ce7a573067a72f876ac
PachydermX <renjimin@gmail.com>
2016-02-10 10:40:33 +0900

    Text Edit

--- katana.php
+++ katana.php
@@ -59,8 +59,10 @@
 			<hr class="title_divider">
 			<div class="context_container">
 				<p class="context">
-					Combining technology, design & media we build advanced connections between brands and their audiences. We create compelling solutions that deliver and sustain business growth, 
-					<span class="mhhide">starting from web and mobile applications to complex business enterprise software. </span>
+					<?php echo get_post_meta($post->ID, "Home Text Compact", true) ?>
+					<span class="mhhide">
+					<?php echo get_post_meta($post->ID, "Home Text Extension", true) ?>
+					</span>
 				</p>
 			</div>
 		</div>
@@ -71,7 +73,9 @@
 		<!-- WEBSITE 1-->
 		<div class="cover" id="websitebg"></div>
 		<div id="websitewbg">
-			<span class="middle title">Digitilizing your Business</span>
+			<span class="middle title">
+				<?php echo get_post_meta($post->ID, "Business Title", true) ?>
+			</span>
 		</div>
 		
 		<div id="websiteslider1" class="preview_container center black">
@@ -80,13 +84,13 @@
 			</div>
 			<div class="title_container">
 				<span class="title second">
-					The Company
+					<?php echo get_post_meta($post->ID, "Business Slider Item 1 Title", true) ?>
 				</span>
 			</div>
 			<hr class="title_divider">
 			<div class="context_container">
 				<p class="context">
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat 
+					<?php echo get_post_meta($post->ID, "Business Slider Item 1 Content", true) ?>
 				</p>
 			</div>
 			<div class="tag_container">
@@ -108,13 +112,13 @@
 			</div>
 			<div class="title_container">
 				<span class="title second">
-					The Company
+					<?php echo get_post_meta($post->ID, "Business Slider Item 2 Title", true) ?>
 				</span>
 			</div>
 			<hr class="title_divider">
 			<div class="context_container">
 				<p class="context">
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat 
+					<?php echo get_post_meta($post->ID, "Business Slider Item 2 Content", true) ?>
 				</p>
 			</div>
 			<div class="tag_container">
@@ -136,13 +140,13 @@
 			</div>
 			<div class="title_container">
 				<span class="title second">
-					The Company
+					<?php echo get_post_meta($post->ID, "Business Slider Item 3 Title", true) ?>
 				</span>
 			</div>
 			<hr class="title_divider">
 			<div class="context_container">
 				<p class="context">
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat 
+					<?php echo get_post_meta($post->ID, "Business Slider Item 3 Content", true) ?>
 				</p>
 			</div>
 			<div class="tag_container">
@@ -164,13 +168,13 @@
 			</div>
 			<div class="title_container">
 				<span class="title second">
-					The Company
+					<?php echo get_post_meta($post->ID, "Business Slider Item 4 Title", true) ?>
 				</span>
 			</div>
 			<hr class="title_divider">
 			<div class="context_container">
 				<p class="context">
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat 
+					<?php echo get_post_meta($post->ID, "Business Slider Item 4 Content", true) ?>
 				</p>
 			</div>
 			<div class="tag_container">
@@ -207,13 +211,13 @@
 			</div>
 			<div class="title_container">
 				<span class="title">
-					Website Creation
+					<?php echo get_post_meta($post->ID, "Website Title", true) ?>
 				</span>
 			</div>
 			<hr class="title_divider">
 			<div class="context_container">
 				<p class="context">
-					Combining technology, design & media we build advanced connections between brands and their audiences. We create compelling solutions that deliver and sustain business growth, starting from web and mobile applications to complex business enterprise software.
+					<?php echo get_post_meta($post->ID, "Website Content", true) ?>
 				</p>
 			</div>
 			<div class="button_container">
@@ -244,19 +248,16 @@
 			</div>
 			<div class="title_container">
 				<span class="title">
-					Interactive materials
+					<?php echo get_post_meta($post->ID, "Interactive Title", true) ?>
 				</span>
 			</div>
 			<hr class="title_divider">
 			<div class="context_container">
 				<p class="context">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat fugiat nulla pariatur.
+					<?php echo get_post_meta($post->ID, "Interactive Content Compact", true) ?>
 				</p>
 				<ul class="context mhhide">
-					<li>Identify the different types of websites available</li>
-					<li>develop an online process of selecting and creating a website</li>
-					<li>Login to an online store and explore product management</li>
-					<li>Identify the different on-line and off-line marking strategies</li>
+					<?php echo get_post_meta($post->ID, "Interactive Content Extension", true) ?>
 				</ul>
 			</div>
 			<div class="button_container black_button">
@@ -281,19 +282,16 @@
 		<div class="preview_container black" id="brandtext">
 			<div class="title_container">
 				<span class="title">
-					Brand Designing
+					<?php echo get_post_meta($post->ID, "Brand Title", true) ?>
 				</span>
 			</div>
 			<hr class="title_divider">
 			<div class="context_container">
 				<p class="context">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat fugiat nulla pariatur.
+					<?php echo get_post_meta($post->ID, "Brand Content Compact", true) ?>
 				</p>
 				<ul class="context mhhide">
-					<li>Identify the different types of websites available</li>
-					<li>develop an online process of selecting and creating a website</li>
-					<li>Login to an online store and explore product management</li>
-					<li>Identify the different on-line and off-line marking strategies</li>
+					<?php echo get_post_meta($post->ID, "Brand Content Extension", true) ?>
 				</ul>
 			</div>
 			<div class="button_container black_button">
@@ -322,13 +320,13 @@
 			</div>
 			<div class="title_container">
 				<span class="title">
-					Video Making
+					<?php echo get_post_meta($post->ID, "Video Title", true) ?>
 				</span>
 			</div>
 			<hr class="title_divider">
 			<div class="context_container">
 				<p class="context">
-					Combining technology, design & media we build advanced connections between brands and their audiences. We create compelling solutions that deliver and sustain business growth, starting from web and mobile applications to complex business enterprise software. 
+					<?php echo get_post_meta($post->ID, "Video Content", true) ?>
 				</p>
 			</div>
 			<div class="thumb_display mhhide">
@@ -337,7 +335,7 @@
 						<img class="thumb" src="<?php echo $template_uri; ?>katana/videosample1.jpg">
 					</div>
 					<div class="thumb_title">
-						Storm Partners
+						<?php echo get_post_meta($post->ID, "Video Thumb 1 Title", true) ?>
 					</div>
 				</div>
 				<div class="thumb_box">
@@ -345,7 +343,7 @@
 						<img class="thumb" src="<?php echo $template_uri; ?>katana/videosample2.jpg">
 					</div>
 					<div class="thumb_title">
-						Studio Katana
+						<?php echo get_post_meta($post->ID, "Video Thumb 2 Title", true) ?>
 					</div>
 				</div>
 			</div>
@@ -365,15 +363,17 @@
 		<div class="cover" id="supportbg"></div>
 		<div id="supportwbg"></div>
 		<div class="title" id="supporttitle">
-			Total Support for you
+			<?php echo get_post_meta($post->ID, "Support Title", true) ?>
 		</div>
 		<div class="supporttext" id="supporttexta">
 			<div class="image_container">
 				<img src="<?php echo $template_uri; ?>katana/supporttexta.png">
 			</div>
 			<p class="context small">
-				Based on a full analysis of your requirements, we uncover the full potential of what your new website can do for you and your business. 
-				<span class="mhhide">We may also offer you a surprize or two which you may dramatically improve your outcome. </span>
+				<?php echo get_post_meta($post->ID, "Support Content 1 Compact", true) ?>
+				<span class="mhhide">
+					<?php echo get_post_meta($post->ID, "Support Content 1 Extension", true) ?>
+				</span>
 			</p>
 		</div>
 		<div class="supporttext" id="supporttextb">
@@ -381,8 +381,10 @@
 				<img src="<?php echo $template_uri; ?>katana/supporttextb.png">
 			</div>
 			<p class="context small">
-			We identify the latest and most suitable web technologies to achieve the result you desire (or better),
-				<span class="mhhide">ensuring the maximum return of your investment whilst providing you the cutting edge online tools for your business. </span>
+				<?php echo get_post_meta($post->ID, "Support Content 2 Compact", true) ?>
+				<span class="mhhide">
+					<?php echo get_post_meta($post->ID, "Support Content 2 Extension", true) ?>
+				</span>
 			</p>
 		</div>
 		<div class="supporttext" id="supporttextc">
@@ -390,8 +392,10 @@
 				<img src="<?php echo $template_uri; ?>katana/supporttextc.png">
 			</div>
 			<p class="context small">
-				Within the first 4 seconds, the visitors to a website decide if they wish to stay or move on to another place in the cyber space. 
-				<span class="mhhide">The Commercial Design Artists at Studio Katana are experts on creating a functional, yet visually attractive websites. We can make your new website the true visual representation of your business, just as unique, just as good looking! </span>
+				<?php echo get_post_meta($post->ID, "Support Content 3 Compact", true) ?>
+				<span class="mhhide">
+					<?php echo get_post_meta($post->ID, "Support Content 3 Extension", true) ?>
+				</span>
 			</p>
 		</div>
 		<div class="supporttext" id="supporttextd">
@@ -399,8 +403,11 @@
 				<img src="<?php echo $template_uri; ?>katana/supporttextd.png">
 			</div>
 			<p class="context small">
-			At Studio Katana, we offer the latest technology but don't charge you an arm and a leg !  
-				<span class="mhhide">To achieve our vision of providing the Global SME community the best online tools at an affordable prices, Studio Katana specializes in creating custom made websites based on WordPress, Joomla and other great cyber tools. What does this mean? Well, if your site was build without taking advantage of those great free resources, it really would cost you an arm and leg. </span>
+				<?php echo get_post_meta($post->ID, "Support Content 4 Compact", true) ?>
+				<span class="mhhide">
+					<?php echo get_post_meta($post->ID, "Support Content 4 Extension", true) ?>
+				</span>
+			  
 			</p>
 		</div>
 		
